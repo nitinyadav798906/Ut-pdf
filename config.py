11bf15342ba2a6e6fc483387a5d8c34faa6fae16
brdfg <hasenorc@gmail.com>
2025-08-29 15:23:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))  
+API_HASH = os.getenv("API_HASH", "xxxxxxxxxxxxxxxx")  
+BOT_TOKEN = os.getenv("BOT_TOKEN", "123456:ABC-DEF...")  
+
+WORKER_URL = os.getenv("WORKER_URL", "https://39217846.utkarsh-proxy.pages.dev/?url=")
