62d6fc1e10ae90f8ca1f5593c19486ab2fe15cbe
nitinyadav798906 <nitinyadav798906@gmail.com>
2025-08-31 16:17:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))  
-API_HASH = os.getenv("API_HASH", "xxxxxxxxxxxxxxxx")  
-BOT_TOKEN = os.getenv("BOT_TOKEN", "123456:ABC-DEF...")  
+API_ID = int(os.getenv("API_ID", "12475131"))  
+API_HASH = os.getenv("API_HASH", "719171e38be5a1f500613837b79c536f")  
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7564571951:AAE6xX7b2wPr6jh2SNV4ZH6EoVJXREuyAU8")  
 
 WORKER_URL = os.getenv("WORKER_URL", "https://39217846.utkarsh-proxy.pages.dev/?url=")
